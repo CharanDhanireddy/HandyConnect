b4ca6c5df5ec9eb21c3ebc40bdec4503cfded8ba
vdhavaleswarapu <venkat20d@yahoo.com>
2022-02-03 01:50:39 -0500

    cleaned up code and added 2 apis - consumer/vendor info, city info, and updated schemas for DB tables

--- backend/dummy.go
+++ backend/dummy.go
@@ -19,7 +19,10 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 func handleRequests() {
 	http.HandleFunc("/", homePage)
 
-	http.HandleFunc("/consumeapi", returnSearch)
+	http.HandleFunc("/consumeapi", returnSearch) //pushing the consumer/vendor info to consumeapi
+	// log.Fatal(http.ListenAndServe(":10000", nil))
+
+	http.HandleFunc("/cityapi", returnCity) //pushing the cities json to cityapi
 	log.Fatal(http.ListenAndServe(":10000", nil))
 }
 
@@ -29,22 +32,41 @@ type search struct {
 	Position string `json: "Position"`
 }
 
+type city struct {
+	City_name string `json: "city_name"`
+	City_id   int    `json: "city_id"`
+}
+
+var cities []city
 var items []search
 
 var id string
 var name string
 var pos string
 
-func returnSearch(w http.ResponseWriter, r *http.Request) {
+var city_id int
+var city_name string
 
-	fmt.Println("Returning the search criteria:")
+//json for customer/vendor information
+
+func returnSearch(w http.ResponseWriter, r *http.Request) { //can be revamped to push customer/vendor info
+
+	fmt.Println("Returning the user search criteria:")
 	json.NewEncoder(w).Encode(items)
+
+}
+
+//json for city information
+
+func returnCity(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("Returning the city search criteria:")
+	json.NewEncoder(w).Encode(cities)
 
 }
 
 func main() {
 	os.Remove("sqlite-database.db") // I delete the file to avoid duplicated records.
-	// SQLite is a file based database.
 
 	log.Println("Creating sqlite-database.db...")
 	file, err := os.Create("sqlite-database.db") // Create SQLite file
@@ -57,6 +79,9 @@ func main() {
 	sqliteDatabase, _ := sql.Open("sqlite3", "./sqlite-database.db") // Open the created SQLite File
 	defer sqliteDatabase.Close()                                     // Defer Closing the database
 	createTable(sqliteDatabase)                                      // Create Database Tables
+
+	createCity(sqliteDatabase)
+	insertCity(sqliteDatabase, "Gainesville")
 
 	// INSERT RECORDS
 	insertStudent(sqliteDatabase, "0001", "Liana Kim", "Bachelor")
@@ -72,8 +97,15 @@ func main() {
 	// DISPLAY INSERTED RECORDS
 	displayStudents(sqliteDatabase)
 
+	//printing city
+	displayCity(sqliteDatabase)
+
 	items = []search{
-		search{ID: id, Name: name, Position: pos},
+		{ID: id, Name: name, Position: pos},
+	}
+
+	cities = []city{
+		{City_id: city_id, City_name: city_name},
 	}
 
 	handleRequests()
@@ -111,13 +143,39 @@ func insertStudent(db *sql.DB, code string, name string, program string) {
 	}
 }
 
+func createCity(db *sql.DB) {
+	createCityTableSQL := `CREATE TABLE city (
+		"city_id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,		
+		"city_name" TEXT	
+	  );` // SQL Statement for Create Table
+
+	log.Println("Create city table...")
+	statement, err := db.Prepare(createCityTableSQL) // Prepare SQL Statement
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	statement.Exec() // Execute SQL Statements
+	log.Println("city table created")
+}
+
+// We are passing db reference connection from main to our method with other parameters
+func insertCity(db *sql.DB, city_name string) {
+	log.Println("Inserting city record ...")
+	insertCitySQL := `INSERT INTO city(city_name) VALUES (?)`
+	statement, err := db.Prepare(insertCitySQL) // Prepare statement.
+	// This is good to avoid SQL injections
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	_, err = statement.Exec(city_name)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+}
+
 func displayStudents(db *sql.DB) {
 
 	sqlStmt := `SELECT code, name, program FROM student WHERE code = $1;`
-
-	// var id string
-	// var name string
-	// var pos string
 
 	row := db.QueryRow(sqlStmt, "0002")
 	switch err := row.Scan(&id, &name, &pos); err {
@@ -128,23 +186,20 @@ func displayStudents(db *sql.DB) {
 	default:
 		panic(err)
 	}
-
-	// row, err := db.Query("SELECT * FROM student ORDER BY name")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// defer row.Close()
-	// for row.Next() { // Iterate and fetch the records from result cursor
-	// 	var id int
-	// 	var code string
-	// 	var name string
-	// 	var program string
-	// 	row.Scan(&id, &code, &name, &program)
-	// 	log.Println("Student: ", code, " ", name, " ", program)
-	// }
-	// row.Close()
 }
 
-// func displayRes(db *sql.DB){
-// 	rows, err := db.Query("SELECT * FROM student")
-// }
+func displayCity(db *sql.DB) {
+
+	sqlStmt := `SELECT city_ID, city_name FROM City WHERE city_ID = $1;`
+
+	row := db.QueryRow(sqlStmt, "1")
+	switch err := row.Scan(&city_id, &city_name); err {
+	case sql.ErrNoRows:
+		fmt.Println("No rows")
+	case nil:
+		fmt.Println(city_id, city_name)
+	default:
+		panic(err)
+	}
+
+}
