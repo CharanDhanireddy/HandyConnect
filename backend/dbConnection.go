0e6be6493c90929dc6c1bd3580594c6efeffe7dd
venkata-shiva-m <manchala.v@ufl.edu>
2022-02-03 17:46:03 -0500

    merge with backend-dev conflicts resolve

---
+++ backend/dbConnection.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	_ "github.com/mattn/go-sqlite3" // Import go-sqlite3 library
+)
+
+func newDBConnection() {
+	os.Remove("sqlite-database.db") // I delete the file to avoid duplicated records.
+
+	log.Println("Creating sqlite-database.db...")
+	file, err := os.Create("sqlite-database.db") // Create SQLite file
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	file.Close()
+	log.Println("sqlite-database.db created")
+
+	sqliteDatabase, _ := sql.Open("sqlite3", "./sqlite-database.db") // Open the created SQLite File
+	defer sqliteDatabase.Close()                                     // Defer Closing the database
+	createTable(sqliteDatabase)                                      // Create Database Tables
+
+	createCityTable(sqliteDatabase)
+
+	createVendorTable(sqliteDatabase)
+
+	populateData(sqliteDatabase)
+}
+
+func createTable(db *sql.DB) {
+	createStudentTableSQL := `CREATE TABLE student (
+		"idStudent" integer NOT NULL PRIMARY KEY AUTOINCREMENT,		
+		"code" TEXT,
+		"name" TEXT,
+		"program" TEXT		
+	  );` // SQL Statement for Create Table
+
+	log.Println("Create student table...")
+	statement, err := db.Prepare(createStudentTableSQL) // Prepare SQL Statement
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	statement.Exec() // Execute SQL Statements
+	log.Println("student table created")
+}
+
+func createCityTable(db *sql.DB) {
+	createCityTableSQL := `CREATE TABLE city (
+		"city_id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,		
+		"city_name" TEXT	
+	  );` // SQL Statement for Create Table
+
+	log.Println("Create city table...")
+	statement, err := db.Prepare(createCityTableSQL) // Prepare SQL Statement
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	statement.Exec() // Execute SQL Statements
+	log.Println("city table created")
+}
+
+func createVendorTable(db *sql.DB) {
+	createVendorTableSQL := `CREATE TABLE vendor (
+		"first_name" TEXT NOT NULL,
+		"last_name" TEXT,
+		"phone" integer,
+		"email" TEXT,
+		"service_1" TEXT,
+		"service_2" TEXT,
+		"service_3" TEXT
+	  );` // SQL Statement for Create Table
+
+	log.Println("Create vendor table...")
+	statement, err := db.Prepare(createVendorTableSQL) // Prepare SQL Statement
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	statement.Exec() // Execute SQL Statements
+	log.Println("Vendor table created")
+}
---
+++ backend/dbOperations.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3" // Import go-sqlite3 library
+)
+
+func populateData(sqliteDatabase *sql.DB) {
+
+	insertCity(sqliteDatabase, "Gainesville")
+
+	// INSERT RECORDS
+	insertStudent(sqliteDatabase, "0001", "Liana Kim", "Bachelor")
+	insertStudent(sqliteDatabase, "0002", "Glen Rangel", "Bachelor")
+	insertStudent(sqliteDatabase, "0003", "Martin Martins", "Master")
+	insertStudent(sqliteDatabase, "0004", "Alayna Armitage", "PHD")
+	insertStudent(sqliteDatabase, "0005", "Marni Benson", "Bachelor")
+	insertStudent(sqliteDatabase, "0006", "Derrick Griffiths", "Master")
+	insertStudent(sqliteDatabase, "0007", "Leigh Daly", "Bachelor")
+	insertStudent(sqliteDatabase, "0008", "Marni Benson", "PHD")
+	insertStudent(sqliteDatabase, "0009", "Klay Correa", "Bachelor")
+
+	insertVendor(sqliteDatabase, "Aaron", "Smith", 3524513872, "asmith@gmail.com", "plumbing", "carpentry", "electrical")
+	insertVendor(sqliteDatabase, "John", "Doe", 3525555555, "jdoe@gmail.com", "plumbing", "", "")
+
+	// DISPLAY INSERTED RECORDS
+	displayStudents(sqliteDatabase)
+
+	//printing city
+	displayCity(sqliteDatabase)
+
+	//printing vendor details
+	displayVendor(sqliteDatabase)
+}
+
+// We are passing db reference connection from main to our method with other parameters
+func insertStudent(db *sql.DB, code string, name string, program string) {
+	log.Println("Inserting student record ...")
+	insertStudentSQL := `INSERT INTO student(code, name, program) VALUES (?, ?, ?)`
+	statement, err := db.Prepare(insertStudentSQL) // Prepare statement.
+	// This is good to avoid SQL injections
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	_, err = statement.Exec(code, name, program)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+}
+
+// We are passing db reference connection from main to our method with other parameters
+func insertCity(db *sql.DB, city_name string) {
+	log.Println("Inserting city record ...")
+	insertCitySQL := `INSERT INTO city(city_name) VALUES (?)`
+	statement, err := db.Prepare(insertCitySQL) // Prepare statement.
+	// This is good to avoid SQL injections
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	_, err = statement.Exec(city_name)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+}
+
+func insertVendor(db *sql.DB, first_name string, last_name string, phone int, email string, service_1 string, service_2 string, service_3 string) {
+	log.Println("Inserting vendor record ...")
+	insertVendorSQL := `INSERT INTO vendor(first_name, last_name, phone, email,service_1, service_2, service_3) VALUES (?,?,?,?,?,?,?)`
+	statement, err := db.Prepare(insertVendorSQL) // Prepare statement.
+	// This is good to avoid SQL injections
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	_, err = statement.Exec(first_name, last_name, phone, email, service_1, service_2, service_3)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+}
+
+func displayStudents(db *sql.DB) {
+
+	sqlStmt := `SELECT code, name, program FROM student WHERE code = $1;`
+
+	row := db.QueryRow(sqlStmt, "0002")
+	switch err := row.Scan(&id, &name, &pos); err {
+	case sql.ErrNoRows:
+		fmt.Println("No rows")
+	case nil:
+		fmt.Println(id, name, pos)
+	default:
+		panic(err)
+	}
+}
+
+func displayCity(db *sql.DB) {
+
+	sqlStmt := `SELECT city_ID, city_name FROM City WHERE city_ID = $1;`
+
+	row := db.QueryRow(sqlStmt, "1")
+	switch err := row.Scan(&city_id, &city_name); err {
+	case sql.ErrNoRows:
+		fmt.Println("No rows")
+	case nil:
+		fmt.Println(city_id, city_name)
+	default:
+		panic(err)
+	}
+
+}
+
+func displayVendor(db *sql.DB) {
+
+	sqlStmt := `SELECT first_name, last_name, phone FROM vendor WHERE service_1 = $1 ;`
+
+	row, err := db.Query(sqlStmt, "plumbing")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer row.Close()
+	for row.Next() { // Iterate and fetch the records from result cursor
+		row.Scan(&f_name, &l_name, &phn)
+		vend_list = append(vend_list, &vendor{f_name, l_name, phn})
+		// vend = []vendor{
+		// 	{First_name: f_name, Last_name: l_name, Phone: phn},
+		// }
+		fmt.Println(f_name, l_name, phn)
+	}
+	row.Close()
+}
