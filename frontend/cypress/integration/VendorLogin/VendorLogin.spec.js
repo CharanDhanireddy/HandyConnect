e07d312894bd6961e30f73d7d52f4a3fa95e264d
srikanthraop <parcha.srikanthrao@gmail.com>
2022-02-27 17:35:56 -0500

    Added tests for Login and VendorLogin

--- frontend/cypress/integration/VendorLogin/VendorLogin.spec.js
+++ frontend/cypress/integration/VendorLogin/VendorLogin.spec.js
@@ -6,19 +6,19 @@ describe('App', () => {
         // so we must tell it to visit our website with the `cy.visit()` command.
         // Since we want to visit the same URL at the start of all our tests,
         // we include it in our beforeEach function so that it runs before each test
-        cy.visit('http://localhost:3000/login')
+        cy.visit('http://localhost:3000/vendorlogin')
     })
 
-    it('Verify login and redirect to home if details are not found', () => {
+    it('Verify vendor login and redirect to home if details are not found', () => {
         cy.get('input[id="emailId"]').should('exist')
         cy.get('input[id="passwordId"]').should('exist')
-        cy.get('button[id="user-login-button"]').should('exist')
+        cy.get('button[id="vendor-login-button"]').should('exist')
         cy.get('#emailId').type('srikanth@email.com');
         cy.get("#emailId").should('have.value', 'srikanth@email.com')
         cy.get('#passwordId').type('12345678');
         cy.get("#passwordId").should('have.value', '12345678')
-        cy.get('#user-login-button').click();
-        cy.url().should('eq', 'http://localhost:3000/')
+        cy.get('#vendor-login-button').click();
+        cy.url().should('eq', 'http://localhost:3000/vendorDashboard')
     })
 
     
