b97ffef65cf4e2caaa62dddacb745cd72fe1405f
srikanthraop <parcha.srikanthrao@gmail.com>
2022-02-28 17:32:55 -0500

    added a new homepage and removed footer

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import { Container } from "react-bootstrap";
 import HomeCarousel from "./HomeComponents/HomeCarousel";
+import HomeJumbotron from "./HomeComponents/HomeJumbotron";
+import Abcd from "./HomeComponents/Abcd";
 
 class Home extends Component {
   render() {
     return (
       <Container>
-        <HomeCarousel />
+        <HomeJumbotron />
+        <Abcd/ >
+        {/* <HomeCarousel /> */}
       </Container>
     );
   }
