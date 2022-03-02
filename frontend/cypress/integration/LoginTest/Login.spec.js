5c6ef970cdf17cfae61862984463473fe345afdd
CharanDhanireddy <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>
2022-03-02 13:50:22 -0500

    Updated the tests after changing city select functionality

--- frontend/cypress/integration/LoginTest/Login.spec.js
+++ frontend/cypress/integration/LoginTest/Login.spec.js
@@ -5,11 +5,13 @@ describe('App', () => {
         cy.visit('login')
     })
 
-    it('Verify login and redirect to home if wrong credentials are used', () => {
+    it('Verify all fields', () => {
         cy.get('input[id="emailId"]').should('exist')
         cy.get('input[id="passwordId"]').should('exist')
         cy.get('button[id="user-login-button"]').should('exist')
+    })
 
+    it('Verify redirect to home if wrong credentials are used', () => {
         cy.get('#emailId').type('srikanth@email.com');
         cy.get("#emailId").should('have.value', 'srikanth@email.com')
         cy.get('#passwordId').type('12345678');
@@ -19,18 +21,14 @@ describe('App', () => {
         cy.url().should('eq', 'http://localhost:3000/')
     })
 
-    it('Verify login and redirect to dashboard if correct credentials are used', () => {
-        cy.get('input[id="emailId"]').should('exist')
-        cy.get('input[id="passwordId"]').should('exist')
-        cy.get('button[id="user-login-button"]').should('exist')
-
+    it('Verify redirect to dashboard if correct credentials are used', () => {
         cy.get('#emailId').type('u2@gmail.com');
         cy.get("#emailId").should('have.value', 'u2@gmail.com')
         cy.get('#passwordId').type('123456789');
         cy.get("#passwordId").should('have.value', '123456789')
 
         cy.get('#user-login-button').click();
-        cy.get('.form-select').should('exist')
+        cy.get('#dashboardContainerId').should('exist')
     })
 
 
