9a0b799d656bf571e64f690f5b425ccbd9ca6cd4
CharanDhanireddy <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>
2022-03-24 22:48:27 -0400

    Added vendor signUp

--- frontend/src/Components/VendorSignup.jsx
+++ frontend/src/Components/VendorSignup.jsx
@@ -6,9 +6,9 @@ import {
   Row,
   Col,
   Form,
+  FormGroup,
   FormControl
 } from "react-bootstrap";
-import { setUserData } from "../util/localStorage";
 import axios from "axios";
 import { BASE_URL } from "../env_setup"
 
@@ -16,21 +16,26 @@ function VendorSignup(props) {
   const [state, setState] = useState({
     firstName: "",
     lastName: "",
-    service: "",
-    city: "",
+    phone: "",
+    // storing service_id in service
+    service: null,
+    // storing city_id in city
+    city: null,
     email: "",
     password: "",
     rePassword: "",
     cityList: [],
-    serviceList: []
+    serviceList: [],
+    errors: {}
   })
+
   const navigate = useNavigate()
 
   useEffect(() => {
     let fetchData = async () => {
-      let city_response = await axios.get(BASE_URL + "city")
-      let service_response = await axios.get(BASE_URL + "service")
-      setState({ ...state, cityList: city_response.data.cities, serviceList: service_response.data.services })
+      let city_response = await axios.get(BASE_URL + "cities")
+      let service_response = await axios.get(BASE_URL + "services")
+      setState({ ...state, cityList: city_response.data, serviceList: service_response.data })
     }
     fetchData();
   }, [])
@@ -39,32 +44,68 @@ function VendorSignup(props) {
     setState({ ...state, [e.target.name]: e.target.value })
   };
 
+  let isValid = () => {
+    let errors = {}
+    const email_pattern = /\S+@\S+/
+    if (!state.firstName) errors['firstName'] = 'Required'
+    if (!state.lastName) errors['lastName'] = 'Required'
+    if (!state.phone) errors['phone'] = 'Required'
+    if (!state.city) errors['city'] = 'Required'
+    if (!state.city) errors['service'] = 'Required'
+    if (!state.email) {
+      errors['email'] = 'Required'
+    }
+    else if (!email_pattern.test(String(state.email).toLowerCase())) {
+      errors['email'] = 'Provide a valid email address'
+    }
+    if (!state.password) {
+      errors['password'] = 'Required'
+    }
+    else if (state.password.length < 8) {
+      errors['password'] = 'Atleast 8 characters'
+    }
+    if (!state.rePassword) {
+      errors['rePassword'] = 'Required'
+    }
+    else if (state.password !== state.rePassword) {
+      errors['rePassword'] = 'Must match the above password'
+    }
+    setState({ ...state, errors })
+    if (Object.keys(errors).length === 0) return true;
+    else return false;
+  }
+
   let onSignupClick = async () => {
+    if (!isValid()) return
     const vendorData = {
-      firstName: state.firstName,
-      lastName: state.lastName,
+      first_name: state.firstName,
+      last_name: state.lastName,
+      phone: state.phone,
+      city_id: parseInt(state.city),
+      service_id: parseInt(state.service),
       email: state.email,
-      password: state.password,
-      rePassword: state.rePassword,
+      password: state.password
     };
 
-    let res = await axios.post(BASE_URL + "vendor/signup", vendorData)
+    let res = await axios.post(BASE_URL + "vendorSignUp", vendorData)
     let data = res.data
     let status = res.status
     console.log(data, status)
     // let token = data?.vendor?.id ? data?.vendor?.id : null
-    setUserData(data?.vendor)
-    navigate('/vendorDashboard')
+    // setVendorData(data?.vendor)
+    if (status == 200)
+      navigate('/vendorLogin')
+    else
+      navigate('/')
   };
 
   return (
     <Container className="center">
       <Row className="">
         <Col md="4" className="mx-auto ">
-          <h1 className="login-signup-heading">Service Provider Signup</h1>
-          <Form>
-            <Form.Group controlId="firstNameId">
-
+          <h1 className="login-signup-heading">Vendor Signup</h1>
+          <Form noValidate>
+            <Form.Group controlId="firstNameId" className="mb-2">
               <Form.Control
                 className="mb-2"
                 type="text"
@@ -72,11 +113,14 @@ function VendorSignup(props) {
                 placeholder="Enter First Name"
                 value={state.firstName}
                 onChange={onChange}
+                isInvalid={state.errors.firstName}
               />
-              <FormControl.Feedback type="invalid"></FormControl.Feedback>
+              <FormControl.Feedback type="invalid">
+                {state.errors.firstName}
+              </FormControl.Feedback>
             </Form.Group>
 
-            <Form.Group controlId="lastNameId">
+            <Form.Group controlId="lastNameId" className="mb-2">
               <Form.Control
                 className="mb-2"
                 type="text"
@@ -84,31 +128,63 @@ function VendorSignup(props) {
                 placeholder="Enter Last Name"
                 value={state.lastName}
                 onChange={onChange}
+                isInvalid={state.errors.lastName}
               />
-              <FormControl.Feedback type="invalid"></FormControl.Feedback>
+              <FormControl.Feedback type="invalid">
+                {state.errors.lastName}
+              </FormControl.Feedback>
             </Form.Group>
 
-            <Form.Group controlId="cityId">
+            <FormGroup controlId="phoneId" className="mb-2">
+              <FormControl
+                className="mb-2"
+                type="text"
+                name="phone"
+                placeholder="Enter Phone Number"
+                value={state.phone}
+                onChange={onChange}
+                isInvalid={state.errors.phone}
+              />
+              <FormControl.Feedback type="invalid">
+                {state.errors.phone}
+              </FormControl.Feedback>
+            </FormGroup>
+
+            <FormGroup controlId="cityId">
               <Form.Select className="mb-2"
-                onChange={(e) => { setState({ ...state, city: e.target.value }) }}
+                onChange={(e) => {
+                  setState({ ...state, city: e.target.value })
+                }}
+                isInvalid={state.errors.city}
               >
-                <option value={null}>Select</option>
+                <option value={null}>Select city</option>
                 {state.cityList.map(city => (
-                  <option key={city.id} value={city.name}>{city.name}</option>
-                ))}
+                  <option key={city.city_id} value={city.city_id}>{city.city_name}</option>
+                )
+                )}
               </Form.Select>
-            </Form.Group>
+              <FormControl.Feedback type="invalid">
+                {state.errors.city}
+              </FormControl.Feedback>
+            </FormGroup>
 
-            <Form.Group controlId="serviceId">
+            <FormGroup controlId="serviceId">
               <Form.Select className="mb-2"
-                onChange={(e) => { setState({ ...state, service: e.target.value }) }}
+                onChange={(e) => {
+                  setState({ ...state, service: e.target.value })
+                }}
+                isInvalid={state.errors.service}
               >
-                <option value={null}>Select</option>
+                <option value={null}>Select service</option>
                 {state.serviceList.map(service => (
-                  <option key={service.id} value={service.name}>{service.name}</option>
-                ))}
+                  <option key={service.service_id} value={service.service_id}>{service.service_name}</option>
+                )
+                )}
               </Form.Select>
-            </Form.Group>
+              <FormControl.Feedback type="invalid">
+                {state.errors.service}
+              </FormControl.Feedback>
+            </FormGroup>
 
             <Form.Group controlId="emailId">
 
@@ -123,34 +199,40 @@ function VendorSignup(props) {
               <FormControl.Feedback type="invalid"></FormControl.Feedback>
             </Form.Group>
 
-            <Form.Group controlId="passwordId">
-
-              <Form.Control
+            <FormGroup controlId="passwordId" className="mb-2">
+              <FormControl
                 className="mb-2"
                 type="password"
                 name="password"
                 placeholder="Enter password"
                 value={state.password}
                 onChange={onChange}
+                isInvalid={state.errors.password}
               />
-              <Form.Control.Feedback type="invalid"></Form.Control.Feedback>
-            </Form.Group>
 
-            <Form.Group controlId="rePasswordId">
+              <FormControl.Feedback type="invalid">
+                {state.errors.password}
+              </FormControl.Feedback>
+            </FormGroup>
 
-              <Form.Control
+            <FormGroup controlId="rePasswordId" className="mb-2">
+              <FormControl
                 className="mb-2"
                 type="password"
                 name="rePassword"
                 placeholder="Re-enter password"
                 value={state.rePassword}
                 onChange={onChange}
+                isInvalid={state.errors.rePassword}
               />
-              <Form.Control.Feedback type="invalid"></Form.Control.Feedback>
-            </Form.Group>
+              <FormControl.Feedback type="invalid">
+                {state.errors.rePassword}
+              </FormControl.Feedback>
+            </FormGroup>
 
           </Form>
           <Button
+            id="signup-button"
             className="mt-3 w-100 btn btn-lg btn" variant="outline-primary"
             color="primary"
             onClick={onSignupClick}
