d36d0f8c22ee1032fb7521ff22b293560e50f8d6
vdhavaleswarapu <89587982+vdhavaleswarapu@users.noreply.github.com>
2022-03-28 13:58:00 -0400

    Added additional functions for test

--- backend/dbConnection/dbConnection.go
+++ backend/dbConnection/dbConnection.go
@@ -35,6 +35,32 @@ func NewDBConnection() *sql.DB {
 	return db
 }
 
+func TestDBConnection() *sql.DB {
+	// os.Remove("sqlite-database.db") // I delete the file to avoid duplicated records.
+
+	// log.Println("Creating sqlite-database.db...")
+	// file, err := os.Create("sqlite-database.db") // Create SQLite file
+	// if err != nil {
+	// 	log.Fatal(err.Error())
+	// }
+	// file.Close()
+	// log.Println("sqlite-database.db created")
+
+	log.Println("Establishing connection to test_database...")
+	db, err := sql.Open("sqlite3", "./test_db.db?_foreign_keys=on") // Open the created SQLite File
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	log.Println("Successfully connected to test database...")
+	sqliteDatabase = db
+
+	// defer sqliteDatabase.Close()                                    // Defer Closing the database
+
+	// populateData(sqliteDatabase)
+
+	return db
+}
+
 func GetDbConnection() *sql.DB {
 	return sqliteDatabase
 }
