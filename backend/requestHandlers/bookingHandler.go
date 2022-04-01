1477c52732cc6ab2ae19b0469d6884a23d7d9421
vdhavaleswarapu <venkat20d@yahoo.com>
2022-03-31 20:43:19 -0400

    updated structs for schema update

--- backend/requestHandlers/bookingHandler.go
+++ backend/requestHandlers/bookingHandler.go
@@ -69,3 +69,52 @@ func CreateBooking(c *gin.Context) {
 
 	c.JSON(http.StatusOK, response)
 }
+
+func CancelBooking(c *gin.Context) {
+
+	var request schema.BookingSchema
+
+	// Try to decode the request body into the struct. If there is an error,
+	// respond to the client with the error message and a 400 status code.
+
+	if err := c.BindJSON(&request); err != nil {
+		log.Println(err)
+		c.JSON(http.StatusBadRequest, "an error occurred while parsing request")
+		return
+	}
+
+	response, err := dbOperations.CancelBooking(request)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, response)
+}
+
+func RescheduleBooking(c *gin.Context) {
+
+	var request schema.BookingSchema
+
+	// Try to decode the request body into the struct. If there is an error,
+	// respond to the client with the error message and a 400 status code.
+
+	if err := c.BindJSON(&request); err != nil {
+		log.Println(err)
+		c.JSON(http.StatusBadRequest, "an error occurred while parsing request")
+		return
+	}
+
+	response1, err := dbOperations.InsertBooking(request)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err.Error())
+		return
+	}
+	response2, err := dbOperations.CancelBooking(request)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err.Error())
+		return
+	}
+	fmt.Println(response2)
+	c.JSON(http.StatusOK, response1)
+}
