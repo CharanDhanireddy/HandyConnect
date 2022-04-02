f1bee4232930dc822676dc674b860834bbec4def
venkata-shiva-m <manchala.v@ufl.edu>
2022-04-01 21:43:32 -0400

    added tests

--- backend/unitTests_test.go
+++ backend/unitTests_test.go
@@ -1,14 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
-	"fmt"
 	"handy/requestHandlers"
 	"handy/structTypes"
 	"net/http"
 	"net/http/httptest"
-	"testing"
 	"net/url"
+	"testing"
 
 	"handy/dbConnection"
 
@@ -31,7 +31,7 @@ func TestCityRet(t *testing.T) {
 	}
 
 	// Check the response body is what we expect.
-	expected := `[{\"city_id\":1,\"city_name\":\"Gainesville\"},{\"city_id\":2,\"city_name\":\"Tampa\"},{\"city_id\":3,\"city_name\":\"Jacksonville\"}]`
+	expected := `[{"city_id":1,"city_name":"Gainesville"},{"city_id":2,"city_name":"Tampa"},{"city_id":3,"city_name":"Jacksonville"},{"city_id":4,"city_name":"Orlando"},{"city_id":5,"city_name":"Miami"},{"city_id":6,"city_name":"Tallahasse"},{"city_id":7,"city_name":"Alachua"},{"city_id":8,"city_name":"Daytona"},{"city_id":9,"city_name":"New York"},{"city_id":10,"city_name":"San Francisco"}]`
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v",
 			rr.Body.String(), expected)
@@ -66,17 +66,15 @@ func TestCust(t *testing.T) {
 	dbConnection.TestDBConnection()
 
 	rr := httptest.NewRecorder()
-	cust_data, _ := gin.CreateTestContext(rr)
-	
-	var params []gin.Param
-	cust_data.Params =  append(params, gin.Param{"customer_id", "1"})
+	c, _ := gin.CreateTestContext(rr)
+
 	var req http.Request
-	cust_data.Request = &req
+	c.Request = &req
 	var url url.URL
 	req.URL = &url
 	req.URL.RawQuery = "customer_id=1"
 
-	requestHandlers.CustData(cust_data)
+	requestHandlers.CustData(c)
 	assert.Equal(t, 200, rr.Code)
 
 	var got structTypes.Cust
@@ -100,18 +98,25 @@ func TestVend(t *testing.T) {
 	dbConnection.TestDBConnection()
 
 	rr := httptest.NewRecorder()
-	vend_data, _ := gin.CreateTestContext(rr)
-	requestHandlers.ReturnVendor(vend_data)
+	c, _ := gin.CreateTestContext(rr)
+
+	var req http.Request
+	c.Request = &req
+	var url url.URL
+	req.URL = &url
+	req.URL.RawQuery = "vendor_id=5"
+
+	requestHandlers.ReturnVendor(c)
 	assert.Equal(t, 200, rr.Code)
 
-	var got []structTypes.Vendor
+	var got structTypes.Vendor
 	err := json.Unmarshal(rr.Body.Bytes(), &got)
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	// Check the response body is what we expect.
-	expected := `[{"first_name":"vendor name","last_name":"vendor lastname","city_name":"Gainesville","phn":3333333333,"email":"vendormail@email.com","service1":"electrician"}]`
+	expected := `{"id":5,"first_name":"vendor name","last_name":"vendor lastname","city_name":"Gainesville","phone":3333333333,"email":"vendormail@email.com","service_name":"Electrician","rating":0,"rating_count":0}`
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v",
 			rr.Body.String(), expected)
@@ -124,8 +129,15 @@ func TestVend(t *testing.T) {
 func TestServices(t *testing.T) {
 	dbConnection.TestDBConnection()
 	rr := httptest.NewRecorder()
-	serv_data, _ := gin.CreateTestContext(rr)
-	requestHandlers.ReturnServiceList(serv_data)
+	c, _ := gin.CreateTestContext(rr)
+
+	var req http.Request
+	c.Request = &req
+	var url url.URL
+	req.URL = &url
+	req.URL.RawQuery = "city_id=1"
+
+	requestHandlers.ReturnServiceList(c)
 	assert.Equal(t, 200, rr.Code)
 
 	var got []structTypes.Service
@@ -135,7 +147,7 @@ func TestServices(t *testing.T) {
 	}
 
 	// Check the response body is what we expect.
-	expected := `[{"serv_id":3,"serv_name":"Carpenter"}]`
+	expected := `[{"service_id":1,"service_name":"Electrician"},{"service_id":2,"service_name":"Plumber"},{"service_id":7,"service_name":"Painter"},{"service_id":3,"service_name":"Carpenter"},{"service_id":4,"service_name":"Welder"},{"service_id":5,"service_name":"Housekeeper"}]`
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v",
 			rr.Body.String(), expected)
@@ -149,8 +161,14 @@ func TestBookingCust(t *testing.T) {
 	dbConnection.TestDBConnection()
 
 	rr := httptest.NewRecorder()
-	book_cust_data, _ := gin.CreateTestContext(rr)
-	requestHandlers.ReturnBookingCust(book_cust_data)
+	c, _ := gin.CreateTestContext(rr)
+	var req http.Request
+	c.Request = &req
+	var url url.URL
+	req.URL = &url
+	req.URL.RawQuery = "customer_id=4"
+
+	requestHandlers.ReturnBookingCust(c)
 	assert.Equal(t, 200, rr.Code)
 
 	var got []structTypes.Booking
@@ -160,7 +178,7 @@ func TestBookingCust(t *testing.T) {
 	}
 
 	// Check the response body is what we expect.
-	expected := `[{"id":1,"vend_name":"vendor name vendor lastname","cust_name":"customer name last name","service_name":"electrician","city":"Gainesville","day":7,"month":2,"year":2022,"address":"2022"},{"id":2,"vend_name":"vendor name vendor lastname","cust_name":"customer name last name","service_name":"electrician","city":"Gainesville","day":9,"month":2,"year":2022,"address":"2022"},{"id":3,"vend_name":"John Smith","cust_name":"customer name last name","service_name":"electrician","city":"Gainesville","day":8,"month":5,"year":2022,"address":"4000SW"}]`
+	expected := `[{"id":5,"vendor_name":"Joshua Weissman","customer_name":"v d","service_name":"Carpenter","city_name":"Tampa","vendor_id":"8","customer_id":"4","service_id":"3","city_id":"2","day":10,"month":3,"year":2022,"address":"34 st dj Tampa 19873289","booking_status":"Confirmed","customer_rating":0,"vendor_rating":0}]`
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v",
 			rr.Body.String(), expected)
@@ -174,8 +192,13 @@ func TestBookingVend(t *testing.T) {
 	dbConnection.TestDBConnection()
 
 	rr := httptest.NewRecorder()
-	book_vend_data, _ := gin.CreateTestContext(rr)
-	requestHandlers.ReturnBookingVend(book_vend_data)
+	c, _ := gin.CreateTestContext(rr)
+	var req http.Request
+	c.Request = &req
+	var url url.URL
+	req.URL = &url
+	req.URL.RawQuery = "vendor_id=5"
+	requestHandlers.ReturnBookingVend(c)
 	assert.Equal(t, 200, rr.Code)
 
 	var got []structTypes.Booking
@@ -185,7 +208,7 @@ func TestBookingVend(t *testing.T) {
 	}
 
 	// Check the response body is what we expect.
-	expected := `[{"id":3,"vend_name":"John Smith","cust_name":"customer name last name","service_name":"electrician","city":"Gainesville","day":8,"month":5,"year":2022,"address":"4000SW"}]`
+	expected := `[{"id":13,"vendor_name":"vendor name vendor lastname","customer_name":"a b","service_name":"Electrician","city_name":"Gainesville","vendor_id":"5","customer_id":"3","service_id":"1","city_id":"1","day":8,"month":3,"year":2022,"address":"edc Gainesville undefined","booking_status":"Confirmed","customer_rating":0,"vendor_rating":0}]`
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v",
 			rr.Body.String(), expected)
@@ -193,4 +216,48 @@ func TestBookingVend(t *testing.T) {
 	act, _ := json.Marshal(got)
 	assert.Equal(t, expected, string(act))
 
+}
+
+func TestCustLoginSuccess(t *testing.T) {
+	dbConnection.TestDBConnection()
+
+	rr := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(rr)
+	var req http.Request
+	var body = []byte(`{"email":"cusemail@email.com", "password":"pass"}`)
+	req = *httptest.NewRequest("POST", "/customerLogin", bytes.NewBuffer(body))
+	c.Request = &req
+
+	requestHandlers.CustomerLogin(c)
+	assert.Equal(t, 200, rr.Code)
+
+	var got structTypes.Cust
+	err := json.Unmarshal(rr.Body.Bytes(), &got)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Check the response body is what we expect.
+	expected := `{"id":1,"first_name":"customer name","last_name":"last name","city_name":"Gainesville","city_id":1,"phone":2222222222,"email":"cusemail@email.com","rating":0,"rating_count":0}`
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+	act, _ := json.Marshal(got)
+	assert.Equal(t, expected, string(act))
+
+}
+
+func TestCustLoginUnauthorized(t *testing.T) {
+	dbConnection.TestDBConnection()
+
+	rr := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(rr)
+	var req http.Request
+	var body = []byte(`{"email":"cusemail@email.com", "password":"wrong_pass"}`)
+	req = *httptest.NewRequest("POST", "/customerLogin", bytes.NewBuffer(body))
+	c.Request = &req
+
+	requestHandlers.CustomerLogin(c)
+	assert.Equal(t, 401, rr.Code)
 }
