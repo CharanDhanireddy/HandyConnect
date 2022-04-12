15cf87d20853f2455ecbde75384fa9c598d34c0f
CharanDhanireddy <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>
2022-04-12 13:00:36 -0400

    Refactored BookingJS file

--- frontend/src/Components/VendorBooking.jsx
+++ frontend/src/Components/VendorBooking.jsx
@@ -58,10 +58,10 @@ function VendorBooking(props) {
                 centered
                 size="lg"
                 show={props.booking != null}>
-                <Modal.Header className = "booking-header">
+                <Modal.Header className="booking-header">
                     Booking details
                 </Modal.Header>
-                <Modal.Body className = "booking-font">
+                <Modal.Body className="booking-font">
                     {Object.keys(BookingData)
                         // .filter(key => !key.includes('id') && !key.includes('password'))
                         .map(key => (
