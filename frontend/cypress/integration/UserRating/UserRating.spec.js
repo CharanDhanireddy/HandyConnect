8bfa828936ebcde97ec6e073efd34b176a513ffd
CharanDhanireddy <drcharaniitg@gmail.com>
2022-04-20 21:03:51 -0400

    Added production URL in env

--- frontend/cypress/integration/UserRating/UserRating.spec.js
+++ frontend/cypress/integration/UserRating/UserRating.spec.js
@@ -1,6 +1,6 @@
 describe("user journey", function () {
     let server
-    it("abc", () => {
+    it("User rating test", () => {
         cy.visit('/login')
         cy.get('#emailId').clear();
         cy.get('#emailId').type('cristiano@email.com');
@@ -12,16 +12,13 @@ describe("user journey", function () {
         cy.wait(3500)
         cy.get('[href="/bookings"]').click();
         cy.wait(1500)
-        cy.get('[data-cy="4/25/2022"] > strong').click();
+        cy.get('[data-cy="4/27/2022"] > strong').click();
         cy.wait(500)
         cy.get('[for="mui-8"]').click();
-        cy.get('#mui-8').check();
         cy.wait(2500)
         cy.get('[data-cy="closeButton"]').click();
         cy.wait(1500)
-        cy.get('[data-cy="4/25/2022"] > strong').click();
-        cy.wait(2500)
-        cy.get('[data-cy="closeButton"]').click();
         cy.get('[role="button"]').click();
-    })  
+
+    })
 })
--- frontend/src/env_setup.js
+++ frontend/src/env_setup.js
@@ -2,6 +2,8 @@
 
 export const DEV_MOCK_SERVER_BASE_URL = 'http://localhost:5000/'
 
-const PRODUCTION_BASE_URL = 'http://localhost:10000/'
+const DEV_BASE_URL = 'http://localhost:10000/'
+
+const PRODUCTION_BASE_URL = 'https://handyconnect.herokuapp.com/'
 
 export const BASE_URL = PRODUCTION_BASE_URL
